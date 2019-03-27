5e70ef82a16152bd519092e67bbd81506d57a646
a1800589 <a1800589@PAOM5015XP17.haagahelia.amk>
2019-03-27 09:35:48 +0200

    start

---
+++ musicstore/src/main/java/swd20/musicstore/App.java
@@ -0,0 +1,13 @@
+package swd20.musicstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
